a7dc1458deb39c1023d40d5da1cd568f50b82a56
John Dave Decano <johndavedecano@gmail.com>
2017-04-03 07:45:46 +0800

    Rebuild codes

--- dist/index.js
+++ dist/index.js
@@ -40,11 +40,17 @@ var TimeAgo = function (_Component) {
             args[_key] = arguments[_key];
         }
 
-        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = TimeAgo.__proto__ || Object.getPrototypeOf(TimeAgo)).call.apply(_ref, [this].concat(args))), _this), _this.updateTime = function () {
+        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = TimeAgo.__proto__ || Object.getPrototypeOf(TimeAgo)).call.apply(_ref, [this].concat(args))), _this), _this.isMounted = false, _this.state = {
+            lastUpdatedAt: null
+        }, _this.updateTime = function () {
             var interval = _this.getInterval();
             if (interval > 0) {
                 setTimeout(_this.updateTime, interval);
-                _this.forceUpdate();
+                if (_this.isMounted) {
+                    _this.setState({
+                        lastUpdatedAt: new Date().getTime()
+                    });
+                }
             }
         }, _temp), _possibleConstructorReturn(_this, _ret);
     }
@@ -52,9 +58,17 @@ var TimeAgo = function (_Component) {
     _createClass(TimeAgo, [{
         key: 'componentDidMount',
         value: function componentDidMount() {
+
+            this.isMounted = true;
+
             if (this.props.isLive) {
                 this.updateTime();
             }
+        }
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            this.isMounted = false;
         }
     }, {
         key: 'getDifference',
